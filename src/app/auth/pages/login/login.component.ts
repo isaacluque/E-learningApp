db90b19ef683a9ace886d2883f6c2b128ba283d5
isaacluque <isaacluque3@gmail.com>
2023-09-10 22:14:32 -0600

    Ya funciona el modal, ahora a consumir la API de usuario normal

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -17,7 +17,7 @@ export class LoginComponent {
 
   public myForm: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+[.][a-z]{2,4}$')]],
-    password: ['',[Validators.required, Validators.minLength(8), Validators.maxLength(12)]],
+    password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]],
   })
 
   login() {
